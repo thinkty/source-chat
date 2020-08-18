ccfe5c4b796f9fbcb90c959b2fa31f4164a3aa6f
thinkty <thinkty@naver.com>
2020-08-18 20:44:06 +0900

    Added basic route and a route for managing the flow

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+/**
+ * This module defines the basic routes for the server's APIs
+ * [GET]
+ * - / : send 200 status
+ *
+ * [POST]
+ * - /flow : handles updating Dialogflow and the state transition table
+ */
+
+const express = require('express');
+const { handleGraph } = require('./flowManager/index');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.sendStatus(200);
+});
+
+router.post('/flow', (req, res, next) => {
+  try {
+    handleGraph(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
