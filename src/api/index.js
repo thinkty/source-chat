6d602611a16550208fb388ac77a70e65e48d7c93
thinkty <thinkty@naver.com>
2020-08-18 22:16:47 +0900

    Added testing error handling route

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@
  * This module defines the basic routes for the server's APIs
  * [GET]
  * - / : send 200 status
+ * - /error : test error handling
  *
  * [POST]
  * - /flow : handles updating Dialogflow and the state transition table
@@ -15,6 +16,14 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   try {
     res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/error', (req, res, next) => {
+  try {
+    throw 'Testing error handling on server';
   } catch (error) {
     next(error);
   }
