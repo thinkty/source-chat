93274ed9f5385f995a93def96bdf276534fa9327
thinkty <thinkty@naver.com>
2020-08-18 23:23:48 +0900

    Added basic graph validation

--- src/api/flowManager/validator.js
+++ src/api/flowManager/validator.js
@@ -7,8 +7,26 @@
 function validate(graph) {
   const { nodes, edges } = graph;
 
-  if (!nodes.length || nodes.length === 0 || !edges.length || edges.length === 0) {
-    throw 'Invalid graph format';
+  basicCheckupOnNodesAndEdges(graph);
+
+
+}
+
+/**
+ * Check whether the graph has valid nodes and edges. The graph must have
+ * atleast 1 node.
+ *
+ * @param graph Graph object
+ */
+function basicCheckupOnNodesAndEdges(graph) {
+  const { nodes, edges } = graph;
+
+  if (!Array.isArray(nodes) || !Array.isArray(edges)) {
+    throw 'Invalid graph format: nodes and edges should be arrays';
+  }
+
+  if (nodes.length < 1) {
+    throw 'Invalid graph format: there should be atleast one node';
   }
 }
 
