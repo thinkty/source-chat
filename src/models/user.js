10ad8ef5419263a3c9e2091450c49e9bc5b23c74
thinkty <thinkty@naver.com>
2020-08-24 22:37:42 +0900

    Added description for user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,10 @@
 const { Schema, model } = require('mongoose');
 
+/**
+ * This schema is intented to keep track of the user status. The user property
+ * must be unique amongst users and various platforms. The state property must
+ * have a valid context value that exists in the Dialogflow console.
+ */
 const UserSchema = new Schema({
   user: { type: String, required: true, unique: true },
   state: { type: String, default: 'root' },
