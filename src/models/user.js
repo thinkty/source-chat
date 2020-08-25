26a0d7d27b5701b511d0602904e32082f6dd98c0
thinkty <thinkty@naver.com>
2020-08-25 12:34:16 +0900

    Updated state of user model to an array of strings

--- src/models/user.js
+++ src/models/user.js
@@ -7,7 +7,7 @@ const { Schema, model } = require('mongoose');
  */
 const UserSchema = new Schema({
   user: { type: String, required: true, unique: true },
-  state: { type: String, default: 'root' },
+  state: { type: [String], default: 'root' },
 }, { timestamps: true });
 
 module.exports = { User: model('User', UserSchema) };
