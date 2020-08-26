c1a0bd0837dde0c22a348ecd6fa83fa2ffc163be
thinkty <thinkty@naver.com>
2020-08-26 13:43:29 +0900

    Added test cases for lookup state table

--- src/tests/stateTable.test.js
+++ src/tests/stateTable.test.js
@@ -1,30 +1,47 @@
 require('dotenv').config();
 const { StateTable } = require('../api/stateTable');
 const graph = require('./sample.json');
-const { INTENT_TYPE } = require('../config/graph');
+const { CONTEXT_TYPE, INTENT_TYPE } = require('../config/graph');
 
 describe('Testing the State Transition Table module', () => {
   it('Should update the state table with the latest intents', async () => {
     await StateTable.update();
 
-    let totalInputContextNum = 0;
+    let contextNodes = 0;
+    graph.nodes.forEach((node) => {
+      const { type } = node;
+      if (type === CONTEXT_TYPE) {
+        contextNodes += 1;
+      }
+    });
+
+    expect(StateTable.table.size).toEqual(contextNodes);
+  });
+
+  it('Should fail when the context does not exist', () => {
+    const states = ['noneExistantState'];
+    const value = 'noneExistantIntentDisplayName';
+
+    expect(StateTable.lookup(states, value).length).toEqual(0);
+  });
+
+  it('Should retrieve the appropriate context', () => {
+    const state = 'root';
+    const id = '0c810a65-d0e8-408d-8218-c3dc7f8fd910';
+    const value = graph.nodes[0].title;
+
+    let rootContexts = 0;
     graph.nodes.forEach((node) => {
       const { type } = node;
       if (type === INTENT_TYPE) {
         const { contexts } = node;
         const { in: inputContexts } = contexts;
-        totalInputContextNum += inputContexts.length;
+        if (inputContexts.includes(id)) {
+          rootContexts += 1;
+        }
       }
     });
 
-    expect(StateTable.table.size).toEqual(totalInputContextNum);
-  });
-
-  it('Should fail when the context does not exist', () => {
-
-  });
-
-  it('Should retrieve the appropriate context', () => {
-
+    expect(StateTable.lookup([state], value).length).toEqual(rootContexts);
   });
 });
